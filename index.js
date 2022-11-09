0fac7e7f208540dd113e7f86ee37f014c91a24d9
Leo Quezada <leofeb12@yahoo.com>
2022-11-09 14:17:44 -0600

    added regular expression validation

--- index.js
+++ index.js
@@ -2,26 +2,56 @@
 
 function getUser(){
 
-//Collect user data
+// 1. Collect user data
     fname = document.getElementById('fname').value;
     lname = document.getElementById('lname').value;
     email = document.getElementById('email').value;
     phone = document.getElementById('phone').value;
     ssn = document.getElementById('ssn').value;
 
-    console.log(ssn.match(/[0-9]/g));
-//Check if value is valid with regular expression (accept multiple formats)
-    //match() method returns null if no match is found. search() searches for a match, and returns the position of the match. replace() returns a modified string where the pattern is replaced.
-    if (ssn.match(/\d\d\d-\d\d-\d\d\d\d/) == null){
-        console.log("Plese use format 123-45-6789");
-   } else{
-        console.log("That works!");
-    }
+// 2.  Validate and format Input
+    // Check if first and last name are words
+    if(fname.match(/[a-z]|[A-Z]/g) != null && fname.match(/[a-z]|[A-Z]/g) != null){
     
-    
+        //Only first letter is uppercase   
+        fname = fname.toLowerCase();
+        lname = lname.toLowerCase();
+        fname = fname.charAt(0).toUpperCase() + fname.slice(1, snn.length-1);
+        fname = fname.charAt(0).toUpperCase() + lname.slice(1, snn.length-1);
 
-    //Capiltalize the users name, format his phone, format ssn with regex functions
-        //need to do
+        console.log("Cool name", fname, lname);
+    } else{
+        console.log("That's not your name lol");
+    }
+
+
+    // Check if SSN is format "123-45-6789" or "123456789"
+        if (ssn.match(/\d\d\d-\d\d-\d\d\d\d/) == null){
+            console.log("Plese use format 123-45-6789");
+        }else{
+    // Turn "123-45-6789" into "123456789"
+            console.log("That SSN works!", ssn.replace(/-/g, ""));
+        }
+        
+    // Just check if email has text/digit plus @  text.com/net/edu/org
+        if(email.match(/(\d|\w)-@-(\d|\w)-.-(\d|\w)/g) != null){
+            console.log("That email works!", email);
+        }else{
+            console.log("Not a valid email")
+        }
+
+
+    // Check if phone is format "123456789"
+        if(phone.match(/\d\d\d\d\d\d\d\d\d/g) != null){
+    // Turn "123-45-6789" into "(123)456-789"
+            ssn = phone.insert(ssn, "(", 0);
+            ssn = phone.insert(ssn, ")", 4);
+            ssn = phone.insert(ssn, "-", 8);
+            console.log("That phone number works.", ssn);
+        } else{
+            console.log("Must be format \"123456789\"")
+        }
+
 
     //turn data into object
     let person = {
