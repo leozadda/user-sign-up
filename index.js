840a44d69e1e0f9ca27757ff52a0e97c849d9328
Leo Quezada <leofeb12@yahoo.com>
2022-11-08 21:33:09 -0600

    added javascript

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function getForm(){
+fname = document.getElementById('fname').onsubmit;
+lname = document.getElementById('lname');
+email = document.getElementById('email');
+phone = document.getElementById('phone');
+ssn = document.getElementById('ssn');
+
+console.log(fname, lname, email, phone, ssn);
+}
