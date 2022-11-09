00dac8061bf826feaac25b6a470ff8bb038b4e85
Leo Quezada <leofeb12@yahoo.com>
2022-11-08 21:45:15 -0600

    added javascript

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
 
-function getForm(){
-fname = document.getElementById('fname').onsubmit;
-lname = document.getElementById('lname');
-email = document.getElementById('email');
-phone = document.getElementById('phone');
-ssn = document.getElementById('ssn');
+function getUser(){
+    fname = document.getElementById('fname').value;
+    lname = document.getElementById('lname').value;
+    email = document.getElementById('email').value;
+    phone = document.getElementById('phone').value;
+    ssn = document.getElementById('ssn').value;
 
-console.log(fname, lname, email, phone, ssn);
+let person = {
+    "first name": fname,
+    "last name": lname,
+    "email": email,
+    "phone number": phone,
+    "social security number": ssn
+    }
+
+    //When sending data to a web server, the data has to be a string.
+    const jsonPerson = JSON.stringify(person);
+    console.log(jsonPerson);
 }
