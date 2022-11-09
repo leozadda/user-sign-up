b69946f6cf4d4ad00126966c3606a12d0db59b1c
Leo Quezada <leofeb12@yahoo.com>
2022-11-08 21:54:30 -0600

    added need to do

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 
 
 function getUser(){
+
+    //Collect user data
     fname = document.getElementById('fname').value;
     lname = document.getElementById('lname').value;
     email = document.getElementById('email').value;
     phone = document.getElementById('phone').value;
     ssn = document.getElementById('ssn').value;
 
-let person = {
+    //Check if value is valid with regular expression (accept multiple formats)
+        //need to do
+
+    //Capiltalize the users name, format his phone, format ssn with regex functions
+        //need to do
+
+    //turn data into object
+    let person = {
     "first name": fname,
     "last name": lname,
     "email": email,
@@ -18,4 +27,7 @@ let person = {
     //When sending data to a web server, the data has to be a string.
     const jsonPerson = JSON.stringify(person);
     console.log(jsonPerson);
+
+    //send data to server (fetch POST)
+        //need to do
 }
